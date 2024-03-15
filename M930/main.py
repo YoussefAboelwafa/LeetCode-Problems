aba33918afc6bf899b22cec94df0cc466f25e2e6
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2024-03-15 17:41:12 +0200

    gitignore

---
+++ M930/main.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numSubarraysWithSum(self, nums, goal):
+        counter = 0
+
+        return 0
+
+
+solution = Solution()
+print(solution.numSubarraysWithSum([1, 0, 1, 0, 1], 2))  # 4
+print(solution.numSubarraysWithSum([0, 0, 0, 0, 0], 0))  # 15

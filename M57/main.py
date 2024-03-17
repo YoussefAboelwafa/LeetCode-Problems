63d76b00c644fc4398ef4f0fabd29e5b0b465bd2
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2024-03-17 04:14:59 +0200

    M57

--- M57/main.py
+++ M57/main.py
@@ -2,12 +2,26 @@ class Solution(object):
     def insert(self, intervals, newInterval):
         result = []
 
+        # 3 cases for newInterval:
+        # 1. New interval is before the current interval
+        # 2. New interval is after the current interval
+        # 3. New interval overlaps with the current interval
+
         for i in range(len(intervals)):
+
+            # New interval is before the current interval
+            # If newInterval[1] < intervals[i][0], then newInterval is before intervals[i]
             if newInterval[1] < intervals[i][0]:
                 result.append(newInterval)
                 return result + intervals[i:]
+
+            # New interval is after the current interval
+            # If newInterval[0] > intervals[i][1], then newInterval is after intervals[i]
             elif newInterval[0] > intervals[i][1]:
                 result.append(intervals[i])
+
+            # New interval overlaps with the current interval
+            # If newInterval[1] >= intervals[i][0] and newInterval[0] <= intervals[i][1], then there is an overlap
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
 

ad0e96243d7b4180e6f237c840ecd4d5507482c0
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2024-03-20 06:22:36 +0200

    ListNode

---
+++ Classes/ListNode.py
@@ -0,0 +1,7 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val) + "->" + str(self.next)
